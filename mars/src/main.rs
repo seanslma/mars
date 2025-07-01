bc199fc0b70a495d535f92e50b36fa1f07738a77
Sean <30691708+seanslma@users.noreply.github.com>
2025-07-01 13:54:40 +1000

    Update main.rs

--- mars/src/main.rs
+++ mars/src/main.rs
@@ -1,4 +1,31 @@
-fn main() {
-    let name = "John";
-    println!("Hello, world! This is {}.", name);
+pub mod opt {
+    pub mod gurobi {
+        pub mod lpsol;
+    }
 }
+
+use std::time::Instant;
+use std::path::PathBuf;
+use core::solver::lpsol::{
+    process_lp_file,
+    // process_sol_file,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = "/test";
+    let lpi = "model.lp";
+    let path_buf = PathBuf::from(path).join(lpi);
+    let input_file = path_buf.as_path();
+
+    let start_time = Instant::now();
+    process_lp_file(input_file)?;
+    let elapsed = start_time.elapsed();
+    println!("LP processing completed in {:.3?}", elapsed);
+
+    Ok(())
+}
+
+// fn main() {
+//     let name = "John";
+//     println!("Hello, world! This is {}.", name);
+// }
